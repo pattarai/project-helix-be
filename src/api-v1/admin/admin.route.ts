b5781370b695b9052122c3356745046a739d9f82
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-02-26 21:58:02 +0530

    Added workshop route

--- src/api-v1/admin/admin.route.ts
+++ src/api-v1/admin/admin.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import Controller from "./admin.controller";
+import workshop from "./workshops/workshop.route";
 
 const admin: Router = Router();
 const controller = new Controller();
 
 
 admin.route("/add").post(controller.addCourse)
+admin.use("/workshop", workshop);
 
 
 export default admin;
